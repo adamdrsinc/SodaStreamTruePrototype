67afb86984ccbc0c50884f03dde44661959fb248
tyler <tylerjallen02@gmail.com>
2024-11-02 13:54:41 -0600

    Created template for Drink Generation page to be used.

---
+++ app/src/main/java/com/example/sodastreamprototyping/GenerateDrinksPage.kt
@@ -0,0 +1,42 @@
+package com.example.sodastreamprototyping
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun GenerateDrinksPage() {
+
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 150.dp),
+        Modifier.padding(10.dp)
+    ) {
+        items(100){ item->
+            Card(
+                Modifier
+                    .fillMaxWidth()
+                    .height(128.dp)
+                    .padding(5.dp)
+                    .clickable(){
+
+                    }){
+                Text("Sprite$item")
+                Text("Durian")
+                Text("Durian")
+                Text("Durian")
+            }
+        }
+    }
+}
+
+
+
