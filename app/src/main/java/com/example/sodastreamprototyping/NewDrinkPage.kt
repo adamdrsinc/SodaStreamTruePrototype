d2c855abde1599e10c58b607a41790bbac42bd62
adamdrsinc <adam.sinc124@gmail.com>
2024-11-03 23:38:13 -0700

    updating implementations

--- app/src/main/java/com/example/sodastreamprototyping/NewDrinkPage.kt
+++ app/src/main/java/com/example/sodastreamprototyping/NewDrinkPage.kt
@@ -6,20 +6,17 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import kotlin.toString
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
