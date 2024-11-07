3a6f29f113e008911bb3965985684713c4a18026
tyler <tylerjallen02@gmail.com>
2024-11-06 18:04:33 -0700

    Updated Generate Drinks page to use new style

--- app/src/main/java/com/example/sodastreamprototyping/GenerateDrinksPage.kt
+++ app/src/main/java/com/example/sodastreamprototyping/GenerateDrinksPage.kt
@@ -5,8 +5,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +15,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.sodastreamprototyping.model.Recipe
 import com.example.sodastreamprototyping.viewModel.GenerateDrinksViewModel
 
 
@@ -24,27 +27,33 @@ fun GenerateDrinksPage() {
     val viewModel : GenerateDrinksViewModel = hiltViewModel()
     val baseArray = LocalContext.current.resources.getStringArray(R.array.drink_bases)
     val flavorArray = LocalContext.current.resources.getStringArray(R.array.drink_flavors)
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 150.dp),
+
+    LazyColumn {
         Modifier.padding(10.dp)
-    ) {
-        items(100){ item->
-            val recipe = viewModel.makeRandDrink()
-            Card(
-                Modifier
-                    .fillMaxWidth()
-                    .height(180.dp)
-                    .padding(5.dp)){
-                Column (Modifier.padding(8.dp)){
-                    Text(baseArray[recipe.base], fontWeight = FontWeight.Bold)
-                    Spacer(modifier = Modifier.height(4.dp))
-                    for (flavor in recipe.getUsedFlavors()){
-                        Text(flavor.second.toString() + "X " + flavorArray[flavor.first])
+        items(baseArray.size){ base->
+            Text(baseArray[base], fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(10.dp))
+            LazyRow {
+                items(20){
+                    val recipe = viewModel.makeRandDrink(Recipe(base))
+                    Card(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(220.dp)
+                            .width(180.dp)
+                            .padding(5.dp)){
+                        Column (Modifier.padding(8.dp)){
+                            Text(baseArray[recipe.base], fontWeight = FontWeight.Bold)
+                            Spacer(modifier = Modifier.height(4.dp))
+                            for (flavor in recipe.getUsedFlavors()){
+                                Text(flavor.second.toString() + "X " + flavorArray[flavor.first])
+                            }
+                        }
                     }
                 }
             }
         }
     }
+
 }
 
 
