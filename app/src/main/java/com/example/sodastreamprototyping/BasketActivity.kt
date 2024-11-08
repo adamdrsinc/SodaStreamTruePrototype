d59dac4d9d5ff91ecee7c6abf2bc38dc8300d07e
tyler <tylerjallen02@gmail.com>
2024-11-08 13:39:12 -0700

    Fixed missing import in BasketActivity.kt

--- app/src/main/java/com/example/sodastreamprototyping/BasketActivity.kt
+++ app/src/main/java/com/example/sodastreamprototyping/BasketActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.*
